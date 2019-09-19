aa558a75254f9a2ffdd7d642610a8189bdd6b76d
Leonid Mishukov <lmishukov@slice.com>
2019-09-19 14:50:28 +0300

    Add logging

--- src/test/java/de/autodoc/suite/SimpleCrudTest.java
+++ src/test/java/de/autodoc/suite/SimpleCrudTest.java
@@ -3,14 +3,20 @@ package de.autodoc.suite;
 import de.autodoc.core.BasePage;
 import de.autodoc.pages.*;
 import io.appium.java_client.android.AndroidDriver;
+import org.apache.log4j.Logger;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
+
+import java.lang.reflect.Method;
 
 import static de.autodoc.util.InteractionMethods.pageContainsText;
 
 public class SimpleCrudTest {
+
+    private static Logger logger = Logger.getLogger(SimpleCrudTest.class);
     private AndroidDriver driver;
     private static final String FIRST_CHOSEN_CAR = "BMW";
     private static final String FIRST_CHOSEN_CAR_SERIES = "1 Hatchback (E87) (02.2003 - 01.2013)";
@@ -27,10 +33,18 @@ public class SimpleCrudTest {
 
     @BeforeClass
     public void setUp() {
+        logger.info("Starting setUp");
         this.driver = new BasePage(driver).build();
         StartPage startPage = new StartPage(driver);
         mainCataloguePage = startPage.clickFrance();
     }
+
+    @BeforeMethod
+    public void handleTestMethodName(Method method) {
+        String testName = method.getName();
+        logger.info("Test " + testName + " starts...");
+    }
+
 
     @Test
     public void createCar1() {
@@ -48,6 +62,7 @@ public class SimpleCrudTest {
 
         mainCataloguePage = vehiclesCataloguePage.addEngine(FIRST_CHOSEN_CAR_ENGINE);
 
+        logger.info("Check that chosen car features are on the main page.");
         softAssert.assertTrue(pageContainsText(driver, FIRST_CHOSEN_CAR));
         softAssert.assertTrue(pageContainsText(driver, FIRST_CHOSEN_CAR_SERIES));
         softAssert.assertTrue(pageContainsText(driver, FIRST_CHOSEN_CAR_ENGINE));
@@ -63,6 +78,7 @@ public class SimpleCrudTest {
         mainCataloguePage.swipeTopViewLeft();
 
         EditCarPage editCarPage = mainCataloguePage.clickEditButton();
+        logger.info("Check that previously chosen car features same as in the Edit View.");
         softAssert.assertEquals(editCarPage.getFirstChosenCarText(), FIRST_CHOSEN_CAR);
         softAssert.assertEquals(editCarPage.getFirstChosenCarModelText(), FIRST_CHOSEN_CAR_SERIES);
         softAssert.assertEquals(editCarPage.getFirstChosenCarEngineText(), FIRST_CHOSEN_CAR_ENGINE);
@@ -73,12 +89,14 @@ public class SimpleCrudTest {
 
         editCarPage = vehiclesCataloguePage.editEngine(EDITED_CHOSEN_CAR_ENGINE);
 
+        logger.info("Check that newly selected car features displayed correctly in the Edit View.");
         softAssert.assertEquals(editCarPage.getFirstChosenCarText(), EDITED_CHOSEN_CAR);
         softAssert.assertEquals(editCarPage.getFirstChosenCarModelText(), EDITED_CHOSEN_CAR_SERIES);
         softAssert.assertEquals(editCarPage.getFirstChosenCarEngineText(), EDITED_CHOSEN_CAR_ENGINE);
 
         mainCataloguePage = editCarPage.clickSaveChanges();
 
+        logger.info("Check that changed car features are on the main page.");
         softAssert.assertTrue(pageContainsText(driver, EDITED_CHOSEN_CAR));
         softAssert.assertTrue(pageContainsText(driver, EDITED_CHOSEN_CAR_SERIES));
         softAssert.assertTrue(pageContainsText(driver, EDITED_CHOSEN_CAR_ENGINE));
@@ -95,6 +113,7 @@ public class SimpleCrudTest {
 
         mainCataloguePage.clickDeleteButton();
 
+        logger.info("Check that button meaning we haven't chosen a single car yet is present");
         softAssert.assertTrue(mainCataloguePage.addCarButtonIsPresent());
 
         softAssert.assertAll();
@@ -130,6 +149,7 @@ public class SimpleCrudTest {
         EditCarPage editCarPage = mainCataloguePage.clickEditButton();
         mainCataloguePage = editCarPage.clickDeleteButton();
 
+        logger.info("Check that button meaning we haven't chosen a single car yet is present");
         softAssert.assertTrue(mainCataloguePage.addCarButtonIsPresent());
 
         softAssert.assertAll();
